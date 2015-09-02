446e80a190dcb95bf7bde58f7f404f2b8d6179dc
Marc Legendre <marc.legendre@inria.fr>
2015-09-02 18:02:17 +0200

    plugins: Rename init*.h files to config.h
    Let's be honest, it was a terrible name: those files are in no way
    related to initialization.
    
    Besides, in the upcoming build system, those files might be generated at
    configure-time, so they can't be referred to relatively to the current
    file.  They are now included like this:
    
        #include <FooPlugin/config.h>
    
    
    Former-commit-id: 7df0f53ba454665213f408b9eb55ebb3b6b64f60

--- config.h
+++ config.h
@@ -22,15 +22,15 @@
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
-#ifndef PLUGINEXAMPLE_INITPLUGIN_H
-#define PLUGINEXAMPLE_INITPLUGIN_H
+#ifndef PLUGINEXAMPLE_CONFIG_H
+#define PLUGINEXAMPLE_CONFIG_H
 
 #include <sofa/helper/system/config.h>
 
 #ifdef SOFA_BUILD_PLUGINEXAMPLE
-#define SOFA_PLUGINEXAMPLE_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#  define SOFA_PLUGINEXAMPLE_API SOFA_EXPORT_DYNAMIC_LIBRARY
 #else
-#define SOFA_PLUGINEXAMPLE_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#  define SOFA_PLUGINEXAMPLE_API SOFA_IMPORT_DYNAMIC_LIBRARY
 #endif
 
-#endif // PLUGINEXAMPLE_INITPLUGIN_H
+#endif
