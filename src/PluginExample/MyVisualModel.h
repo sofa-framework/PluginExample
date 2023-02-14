7567afdb787904f808d859b79e833dc528dcdece
Hugo <hugo.talbot@sofa-framework.org>
2023-02-14 22:09:02 +0100

    Update path following NG MyVisualModel (#3)
    * Update path
    
    * Fix namespace

--- src/PluginExample/MyVisualModel.h
+++ src/PluginExample/MyVisualModel.h
@@ -24,7 +24,7 @@
 #include <PluginExample/config.h>
 
 #include <sofa/core/BehaviorModel.h>
-#include <SofaBaseVisual/VisualModelImpl.h>
+#include <sofa/component/visual/VisualModelImpl.h>
 
 
 namespace sofa::component::visualmodel
@@ -33,11 +33,11 @@ namespace sofa::component::visualmodel
 /**
  * This VisualModel does not do anything but have a dependency on a module (SofaBaseVisual).
  */
-class SOFA_PLUGINEXAMPLE_API MyVisualModel : public sofa::component::visualmodel::VisualModelImpl
+class SOFA_PLUGINEXAMPLE_API MyVisualModel : public sofa::component::visual::VisualModelImpl
 {
 
 public:
-    SOFA_CLASS(MyVisualModel, sofa::component::visualmodel::VisualModelImpl);
+    SOFA_CLASS(MyVisualModel, sofa::component::visual::VisualModelImpl);
 
 protected:
     MyVisualModel();
